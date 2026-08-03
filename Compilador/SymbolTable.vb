f760632a87edf312a83bc1b893276b554f689713
Jeg <joelescanoguzman@gmail.com>
2026-08-03 11:33:54 -0400

    Modificar tabla de simbolos para que manehhje una pila de diccionarios para q las variables declaradas existan solo dentro de un bloque

--- Compilador/SymbolTable.vb
+++ Compilador/SymbolTable.vb
@@ -13,40 +13,74 @@ Public Class Variable
 End Class
 
 Public Class SymbolTable
-    Private variables As Dictionary(Of String, Variable)
+    ' Pila de diccionarios: el último elemento es el ámbito actual
+    Private scopes As Stack(Of Dictionary(Of String, Variable))
 
     Public Sub New()
-        variables = New Dictionary(Of String, Variable)
+        scopes = New Stack(Of Dictionary(Of String, Variable))
+        PushScope() ' Ámbito global
     End Sub
 
+    ' Crea un nuevo ámbito y lo apila
+    Public Sub PushScope()
+        scopes.Push(New Dictionary(Of String, Variable))
+    End Sub
+
+    ' Elimina el ámbito actual (se pierden las variables locales)
+    Public Sub PopScope()
+        If scopes.Count > 1 Then
+            scopes.Pop()
+        End If
+    End Sub
+
+    ' Declara variable en el ámbito actual
     Public Sub DeclararVariable(nombre As String, tipo As String)
-        If variables.ContainsKey(nombre) Then
-            Throw New Exception("Variable '" & nombre & "' has already been declared")
+        If scopes.Peek().ContainsKey(nombre) Then
+            Throw New Exception("Variable '" & nombre & "' ya fue declarada en este ámbito.")
         End If
-        variables.Add(nombre, New Variable(nombre, tipo))
+        scopes.Peek().Add(nombre, New Variable(nombre, tipo))
     End Sub
 
+    ' Busca la variable desde el ámbito más interno hacia el global
     Public Function ObtenerVariable(nombre As String) As Variable
-        If Not variables.ContainsKey(nombre) Then
-            Throw New Exception("Variable '" & nombre & "' has not been declared")
-        End If
-        Return variables(nombre)
+        For Each scope In scopes
+            If scope.ContainsKey(nombre) Then
+                Return scope(nombre)
+            End If
+        Next
+        Throw New Exception("Variable '" & nombre & "' no ha sido declarada.")
     End Function
 
+    ' Asigna valor buscando en todos los ámbitos
     Public Sub AsignarValor(nombre As String, valor As Object)
-        Dim variable = ObtenerVariable(nombre)
-        variable.Valor = valor
-        variable.Inicializada = True
+        For Each scope In scopes
+            If scope.ContainsKey(nombre) Then
+                Dim var = scope(nombre)
+                var.Valor = valor
+                var.Inicializada = True
+                Return
+            End If
+        Next
+        Throw New Exception("Variable '" & nombre & "' no ha sido declarada.")
     End Sub
 
+    ' Verifica existencia
     Public Function VariableExiste(nombre As String) As Boolean
-        Return variables.ContainsKey(nombre)
+        For Each scope In scopes
+            If scope.ContainsKey(nombre) Then
+                Return True
+            End If
+        Next
+        Return False
     End Function
 
+    ' Verifica inicialización
     Public Function EstaInicializada(nombre As String) As Boolean
-        If Not variables.ContainsKey(nombre) Then
-            Return False
-        End If
-        Return variables(nombre).Inicializada
+        For Each scope In scopes
+            If scope.ContainsKey(nombre) Then
+                Return scope(nombre).Inicializada
+            End If
+        Next
+        Return False
     End Function
 End Class
